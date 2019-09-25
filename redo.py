75ecabd6310e9e9488d88e5d6271c37016d8fe9c
KatoMono Enkeli <k4t0mono@gmail.com>
2019-09-25 15:31:14 -0300

    redo 2

--- redo.py
+++ redo.py
@@ -4,7 +4,7 @@ import pickle
 from sys import argv
 from ctrl import Tweet_Ctrl
 from utils import get_tweepy_api, get_connection, notify
-from models import Tweet
+from models import Tweet, User
 
 
 logging.config.fileConfig(
@@ -21,28 +21,44 @@ if __name__ == "__main__":
     ctrl_tweet = Tweet_Ctrl()
     redo = pickle.load(open('redo.pkl', 'rb'))
 
-    for i in range(1, 32):
+    dbs = [1,3,5,6,7,8,9,11,12,14,15,16,17,18,20,21,22,23,24,25,26,27,28,29,30,31]
+
+    for i in dbs:
         db = 'hta_{:02}'.format(i)
-        _, s = get_connection(db, uri="mysql+pymysql://hta:Lux.3a@oxum.stuffium.tk")
+        b, s = get_connection(db, uri="mysql+pymysql://hta:Lux.3a@oxum.stuffium.tk")
         logger.info('Working on database {}'.format(db))
 
+        tweets = []
         for i, id_ in enumerate(redo[db]):
             try:
                 status = tweepy.get_status(id_, tweet_mode="extended")
-                t = ctrl_tweet.new_tweet(status)
-                s.add(t.user)
-                s.query(Tweet).filter(Tweet.id == t.id).update({'user_id': t.user.id})
 
             except Exception as e:
                 logger.error('Raise error on {}'.format(id_))
                 logger.error(e)
-                s.rollback()
                 continue
         
-            if (i % 10) == 0:
+            tweets.append(ctrl_tweet.new_tweet(status))
+            if (i % 5) == 0:
                 s.commit()
                 logger.info('I had done {}'.format(i))
         
+        users_redo = [ x.user for x in tweets ]
+        users_db = [ x[0] for x in s.query(User.id).all() ]
+        users = [ x for x in users_redo if str(x.id) not in users_db ]
+        logger.info('Got the users')
+
+        s.bulk_save_objects(users)
+        s.commit()
+        logger.info('commited the users')
+
+        tweets_db = [ x[0] for x in s.query(Tweet.id).filter(Tweet.user_id == None) ]
+        tweets = [ x for x in tweets if str(x.id) in tweets_db ]
+        for t in tweets:
+            s.query(Tweet).filter(Tweet.id == t.id).update({'user_id': t.user.id})
+        s.commit()
+        logger.info('commited tweets')
+
         notify('Database {}'.format(db), 'It is done')
 
     notify('Redo Done', 'Everything should be normal now')
