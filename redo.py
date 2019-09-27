29845ffd35e479b38d5b657d93113db3c4578605
KatoMono Enkeli <k4t0mono@gmail.com>
2019-09-27 15:40:20 -0300

    It is working  	.｡･ﾟﾟ･(＞_＜)･ﾟﾟ･｡.

--- redo.py
+++ redo.py
@@ -5,6 +5,7 @@ from sys import argv
 from ctrl import Tweet_Ctrl
 from utils import get_tweepy_api, get_connection, notify
 from models import Tweet, User
+from subprocess import run
 
 
 logging.config.fileConfig(
@@ -24,19 +25,19 @@ def add_one(s, item):
         logger.error(e)
         s.rollback()
 
-def bulk_add(s, items):
-    try:
-        s.bulk_save_objects(items)
-        s.commit()
 
-    except Exception as e:
-        logger.error(e)
-        s.rollback()
-
-        for i in items:
-            add_one(s, i)
+def divide_chunks(l, n): 
+    for i in range(0, len(l), n):  
+        yield l[i:i + n] 
 
 
+def add_chunk(session, items):
+    chunks = list(divide_chunks(items, 42))
+    for i, chunk in enumerate(chunks):
+        for item in chunk:
+            session.add(item)
+        session.commit()
+        logger.info('Chunk {} done'.format(i))
 
 if __name__ == "__main__":
     logger.info('yo')
@@ -51,8 +52,15 @@ if __name__ == "__main__":
         b, s = get_connection(db)
         logger.info('Working on database {}'.format(db))
 
+        db2 = 'r_{}'.format(db)
+        _, s2 = get_connection(db2)
+        run(('python', 'create_db.py', db2, db2))
+
+        tweets_db = [ t.id for t in s.query(Tweet).all() ]
+        logger.info('Got the tweets')
+
         tweets = []
-        for i, id_ in enumerate(redo[db][:12]):
+        for i, id_ in enumerate(tweets_db):
             try:
                 status = tweepy.get_status(id_, tweet_mode="extended")
                 tweets.append(ctrl_tweet.new_tweet(status))
@@ -63,26 +71,9 @@ if __name__ == "__main__":
                 continue
         
             if (i % 5) == 0:
-                s.commit()
                 logger.info('I had done {}'.format(i))
         
-        users_redo = [ x.user for x in tweets ]
-        users_db = [ x[0] for x in s.query(User.id).all() ]
-        users = [ x for x in users_redo if str(x.id) not in users_db ]
-        logger.info('Got the users')
-        
-        bulk_add(s, users)
-        s.commit()
-        logger.info('commited the users')
-
-        tweets_db = [ x[0] for x in s.query(Tweet.id).filter(Tweet.user_id == None) ]
-        tweets = [ x for x in tweets if str(x.id) in tweets_db ]
-
-        # s.bulk_save_objects(tweets, update_changed_only=False)
-        for t in tweets:
-            s.query(Tweet).filter(Tweet.id == t.id).update({'user_id': t.user.id})
-        s.commit()
-        logger.info('commited tweets')
+        add_chunk(s2, tweets)
 
         notify('Database {}'.format(db), 'It is done')
 
